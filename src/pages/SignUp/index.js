67988440b8b70e08e2a1677df82761e265a57864
Marcus - PC UNIPORT <m.viniciuz@hotmail.com>
2020-01-30 17:28:34 -0300

    fix: final

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Input, Form} from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data){
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="gobarber" />
-      <form>
-        <input type="text" placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Criar conta </button>
         <Link to="/">Já tenho Login</Link>
-      </form>
+      </Form>
     </>
   );
 }
