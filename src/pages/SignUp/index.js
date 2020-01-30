a45b32bb764cca77f13b1942223b84227e37bfbc
Marcus Vinicius <m.viniciuz@hotmail.com>
2020-01-30 20:24:57 -0300

    fix: Configuração inicial redux

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Input, Form} from '@rocketseat/unform';
+import { Input, Form } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .required('A senha é obrigatória')
+    .min(6, 'Senha com no mínimo 6 digitos')
+});
+
 export default function SignUp() {
-  function handleSubmit(data){
+  function handleSubmit(data) {
     console.tron.log(data);
   }
 
   return (
     <>
       <img src={logo} alt="gobarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" type="text" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
